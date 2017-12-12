fd1c0f79390c109fd728714ed46d23ea91559426
dmitrygl <dm.phns@gmail.com>
2017-12-13 01:41:23 +0200

    add interaction with mysql

---
+++ scripts/init_db.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+import mysql.connector
+import datetime
+import subprocess
+
+# connectiong to database;
+cnx = mysql.connector.connect(user='puser', password='pU2ZerP', host='localhost', database='pinganalizdb')
+cursor = cnx.cursor(buffered = True)
+
+# checkign tables in DB. If DB empty, then will make tables, if tables exist - will add data. 
+check = ("SHOW TABLES")
+cursor.execute(check)
+check = cursor.fetchall()
+if not check:
+	TABLES = {}
+	TABLES['pnga_hostname'] = (
+        	"CREATE TABLE `pnga_hostname` ("
+	        "`id` int NOT NULL AUTO_INCREMENT,"
+        	"`hostname` varchar(18) NOT NULL,"
+        	"UNIQUE (`hostname`),"
+        	"PRIMARY KEY (`id`)"
+        	") ENGINE=InnoDB")
+
+	TABLES['pnga_status'] = (
+        	"CREATE TABLE `pnga_status` ("
+	        "`id` int NOT NULL AUTO_INCREMENT,"
+	        "`status` varchar(10) NOT NULL,"
+	        "UNIQUE (`status`),"
+       		"PRIMARY KEY (`id`)"
+	        ") ENGINE=InnoDB")
+
+	TABLES['pnga_data'] = (
+        	"CREATE TABLE `pnga_data` ("
+	        "`id` int NOT NULL AUTO_INCREMENT,"
+	        "`day` int NOT NULL,"
+	        "`month` int NOT NULL,"
+	        "`time` timestamp default current_timestamp,"
+	        "`hostname` int NOT NULL,"
+	        "`status` int NOT NULL,"
+	        "PRIMARY KEY (`id`)"
+	        ") ENGINE=InnoDB")
+
+	for name, ddl in TABLES.items():
+		cursor.execute(ddl)
+	
+	temp = ['good', 'bad', 'attention']
+	# add data in static table
+	for t in temp:
+		addLine = ("INSERT INTO pnga_status (status) VALUES ('{}')".format(t))
+		cursor.execute(addLine)
+	cnx.commit()
+
+else:
+	# similar like ping_analizer.py, but data add in table DB.
+	date = datetime.datetime.now()
+	day = date.day
+	month = date.month	
+	f = open('../file', 'r')
+	for line in f:
+		temp = line.split('=')
+		namedev = temp[0]
+		ipaddr = temp[1]
+		mac = temp[2]
+			
+		searchHost = ("SELECT * FROM pnga_hostname WHERE hostname = '{}'".format(namedev))
+		cursor.execute(searchHost)
+		searchHost = cursor.fetchall()
+		if not searchHost:
+			add = ("INSERT INTO pnga_hostname (hostname) VALUES ('{}')".format(namedev))
+			cursor.execute(add)
+			cnx.commit()
+
+		ping = subprocess.call(['ping', '-c', '2', ipaddr], stdout=subprocess.DEVNULL)
+		macSearch = subprocess.check_output(['ip', 'neigh'])
+		trMacSearch = macSearch.decode()
+		macArg1 = trMacSearch.find(mac)
+		macArg2 = trMacSearch.find(ipaddr)
+
+		if ping == 0:
+			if macArg1 != -1:
+				if macArg1 - macArg2 < 50:
+					searchHost = ("SELECT * FROM pnga_hostname WHERE hostname = '{}'".format(namedev))
+					cursor.execute(searchHost)
+					searchHost = cursor.fetchall()
+					searchHost = searchHost[0]
+					add = ("INSERT INTO pnga_data (day, month, hostname, status) VALUES ('{}', '{}', '{}', '1')".format(day, month, searchHost[0]))
+					cursor.execute(add)
+					cnx.commit()
+				else:
+					searchHost = ("SELECT * FROM pnga_hostname WHERE hostname = '{}'".format(namedev))
+					cursor.execute(searchHost)
+					searchHost = cursor.fetchall()
+					searchHost = searchHost[0]
+					add = ("INSERT INTO pnga_data (day, month, hostname, status) VALUES ('{}', '{}', '{}', '3')".format(day, month, searchHost[0]))
+					cursor.execute(add)
+					cnx.commit()
+			else:
+				searchHost = ("SELECT * FROM pnga_hostname WHERE hostname = '{}'".format(namedev))
+				cursor.execute(searchHost)
+				searchHost = cursor.fetchall()
+				searchHost = searchHost[0]
+				add = ("INSERT INTO pnga_data (day, month, hostname, status) VALUES ('{}', '{}', '{}', '2')".format(day, month, searchHost[0]))
+				cursor.execute(add)
+				cnx.commit()
+		else:
+			searchHost = ("SELECT * FROM pnga_hostname WHERE hostname = '{}'".format(namedev))
+			cursor.execute(searchHost)
+			searchHost = cursor.fetchall()
+			searchHost = searchHost[0]
+			add = ("INSERT INTO pnga_data (day, month, hostname, status) VALUES ('{}', '{}', '{}', '2')".format(day, month, searchHost[0]))
+			cursor.execute(add)
+			cnx.commit()
+
+	f.close()
+
+cursor.close()
+cnx.close()
+
---
+++ scripts/request_db.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import mysql.connector
+from mysql.connector import errorcode
+import cgi, cgitb
+
+# started work with html forms
+form = cgi.FieldStorage()
+
+# recive data from form
+request = form.getvalue("request")
+day = form.getvalue("day")
+month = form.getvalue("month")
+
+link = '<a href="http://pinganalizer/index.html">back</a>'
+
+# connection with DB
+cnx = mysql.connector.connect(user='puser', password='pU2ZerP', host='localhost', database='pinganalizdb')
+cursor = cnx.cursor()
+
+print("Content-type: text/html\n")
+print("""<!DOCTYPE HTML><html><head><meta charset="utf-8"><title>DB report</title></head><body>""")
+
+if request is None or day is None or month is None:
+	print("""<h1 align=center style="color:red">"You must choose day and month, and what kind report you want receive - short or long'</h1>""")
+
+# translate str in int
+request = int(request)
+day = int(day)
+month = int(month)
+
+# if query to DB is short statistic
+if request == 1:
+	# send requests to DB for good (1), bad (2) and attention (3) status.
+	goodQuery = ("SELECT COUNT(*) FROM pnga_data WHERE day = {} AND month = {} and status = 1".format(day, month))
+	cursor.execute(goodQuery)
+	goodQuery = cursor.fetchall() 
+	goodQuery = goodQuery[0]
+	badQuery = ("SELECT COUNT(*) FROM pnga_data WHERE day = {} AND month = {} AND status = 2".format(day, month))
+	cursor.execute(badQuery)
+	badQuery = cursor.fetchall() 
+	badQuery = badQuery[0]
+	attentionQuery = ("SELECT COUNT(*) FROM pnga_data WHERE day = {} AND month = {} AND status = 3".format(day, month))
+	cursor.execute(attentionQuery)
+	attentionQuery = cursor.fetchall() 
+	attentionQuery = attentionQuery[0]
+	
+	cursor.close()
+	cnx.close()
+
+	#print data in browser
+	print("<h1 align=center>During day of {} of month {}:</h1>".format(day, month))
+	print("""<h1 align=center style="color:green">{} hosts wos available</h1>""".format(goodQuery[0]))
+	print("""<h1 align=center style="color:red">{} hosts wos unavailable</h1>""".format(badQuery[0]))
+	print("""<h1 align=center style="color:orange">{} hosts wos need attention</h1>""".format(attentionQuery[0]))
+
+	print("{}" .format(link))
+	
+# if query to DB is long statistic
+else:
+	
+	query = ("SELECT pnga_data.time, pnga_hostname.hostname, pnga_status.status FROM pnga_data LEFT JOIN pnga_hostname ON pnga_hostname.id = pnga_data.hostname LEFT JOIN pnga_status ON pnga_status.id = pnga_data.status WHERE day = {} AND month = {}".format(day, month))
+	cursor.execute(query)
+	query = cursor.fetchall() 
+
+	
+	print("<h1 align=center>During day of {} of month {}:</h1>".format(day, month))
+
+	for y in query:
+		print("<p align=center> {}  </p>".format(y))
+
+	print("{}" .format(link))
+
+cursor.close()
+cnx.close()
+
+print("</body></html>")
+
